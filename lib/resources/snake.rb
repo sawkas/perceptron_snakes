139ba7922050cbbe663201e2a9642d29b230af71
Aleksandr <siretna@gmail.com>
2022-04-19 13:11:14 +0700

    Number of steps without eating apple was was limited & random place for snake generation

--- lib/resources/snake.rb
+++ lib/resources/snake.rb
@@ -8,30 +8,34 @@ module PerceptronSnakes
       BodyCell = Struct.new(:x, :y)
 
       DIRECTIONS = %i[up right down left].freeze
+      POSSIBLE_STEPS_WITHOUT_APPLE = 100
 
       attr_accessor :direction
-      attr_reader :body, :alive, :apples, :steps
+      attr_reader :body, :alive, :apples, :steps, :steps_without_apple
 
       def initialize
-        @body = Array[BodyCell.new(0, 0), BodyCell.new(-1, 0)]
         @direction = DIRECTIONS.sample
+        @body = build_body
         @alive = true
         @steps = 0
         @apples = 0
+        @steps_without_apple = 0
       end
 
       #  Main method
       def step(wall, apple)
         new_head = build_new_head
 
-        if wall.contains?(new_head.x, new_head.y) || body.include?(new_head)
+        if wall.contains?(new_head.x, new_head.y) || body.include?(new_head) || POSSIBLE_STEPS_WITHOUT_APPLE == steps_without_apple
           @alive = false
         else
           if apple.x == new_head.x && apple.y == new_head.y
             @apples += 1
+            @steps_without_apple = 0
 
             apple.new_coordinates(self)
           else
+            @steps_without_apple += 1
             body.pop
           end
 
@@ -52,10 +56,37 @@ module PerceptronSnakes
       end
 
       def calculate_fitness
-        steps + (2**apples + (apples**2.1) * 500) - ((apples**1.2) * (steps * 0.25)**1.3)
+        steps + ((2**apples) + (apples**2.1) * 500) - (((0.25 ** steps)**1.3) * (apples**1.2))
       end
 
       private
+
+      def build_body
+        coordinates = case direction
+        when :up
+          head_x = rand((-Settings.sizes.vector+1)..(Settings.sizes.vector-1))
+          head_y = rand((-Settings.sizes.vector+4)..Settings.sizes.vector-1)
+
+          [[head_x, head_y], [head_x, head_y-1], [head_x, head_y-2]]
+        when :down
+          head_x = rand((-Settings.sizes.vector+1)..(Settings.sizes.vector-1))
+          head_y = rand((-Settings.sizes.vector+1)..(Settings.sizes.vector-4))
+
+          [[head_x, head_y], [head_x, head_y+1], [head_x, head_y+2]]
+        when :right
+          head_x = rand((-Settings.sizes.vector+4)..(Settings.sizes.vector-1))
+          head_y = rand((-Settings.sizes.vector+1)..(Settings.sizes.vector-1))
+
+          [[head_x, head_y], [head_x-1, head_y], [head_x-2, head_y]]
+        when :left
+          head_x = rand((-Settings.sizes.vector+1)..(Settings.sizes.vector-4))
+          head_y = rand((-Settings.sizes.vector+1)..(Settings.sizes.vector-1))
+
+          [[head_x, head_y], [head_x+1, head_y], [head_x+2, head_y]]
+        end
+
+        coordinates.map { |x, y| BodyCell.new(x, y) }
+      end
 
       def build_new_head
         new_head = head.dup
